5d3734cf9669a192c7b7bd3ccb875eadf4bc5cab
aweliego <amelie.gorry.am@gmail.com>
2022-04-20 21:48:20 +0200

    Store hard coded workouts in App's state and pass them as props to WorkoutList and Workout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from '../Form/Form';
 import WorkoutList from '../WorkoutList/WorkoutList';
+import { generateId } from '../../util/utilities';
+//import { workouts } from '../../util/workouts';
 
 const App = () => {
+  const [workouts, setWorkouts] = useState([
+    {
+      id: generateId(),
+      category: 'Sweat Box',
+      type: 'cardio',
+      focus: 'above the belt',
+      level: 'advanced',
+      equipment: 'heavyweights',
+      duration: 'long',
+      videoSrc:
+        'https://christadipaolo.com/boxing-and-bubbles/player.php?data_load_id=249',
+      imgSrc:
+        'https://christadipaolo.com/boxing-and-bubbles/images/thumb/new_thumbs/1612726765.jpg',
+    },
+    {
+      id: generateId(),
+      category: 'Heavyweight',
+      type: 'strength',
+      focus: 'full body',
+      level: 'intermediate',
+      equipment: 'heavyweights',
+      duration: 'average',
+      videoSrc:
+        'https://christadipaolo.com/boxing-and-bubbles/player.php?data_load_id=290',
+      imgSrc:
+        'https://christadipaolo.com/boxing-and-bubbles/images/thumb/new_thumbs/1628446683.jpg',
+    },
+  ]);
+
   return (
     <div className="App">
       <h1>B&B Workout Generator</h1>
       <Form />
-      <WorkoutList />
+      <WorkoutList workouts={workouts} />
     </div>
   );
 };
--- src/components/WorkoutList/WorkoutList.js
+++ src/components/WorkoutList/WorkoutList.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './WorkoutList.css';
 import Workout from '../Workout/Workout';
 
-const WorkoutList = () => {
+const WorkoutList = (props) => {
   return (
-    <div className="">
-      <Workout />
+    <div className="WorkoutList">
+      <h2>Here's what we suggest:</h2>
+      <div>
+        {props.workouts.map((workout) => (
+          <Workout key={workout.id} workout={workout} />
+        ))}
+      </div>
     </div>
   );
 };
